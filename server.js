dc160e53b6ab50855d46d2be9f02433a02006b9e
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-07-11 23:31:07 -0400

    created routes

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+//Data base connection
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
+//routes
+app.use("api/users", require("./routes/api/users"));
+app.use("api/auth", require("./routes/api/auth"));
+app.use("api/profile", require("./routes/api/profile"));
+app.use("api/users", require("./routes/api/users"));
 
 const PORT = process.env.port || 5000;
 
