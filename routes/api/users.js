dfbbbe175ab032cc0d8c614ccf7706320a4bf476
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2020-05-06 07:02:27 -0400

    to clear hd

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 //@route  POST api/users
 //@desc   register user
 //@access Public
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("User route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Name is requiered")
+      .not()
+      .isEmpty("password")
+      .withMessage("please provide a password"),
+    check("email", "Please provide a valid email address").isEmail(),
+    check("password")
+      .isLength({ min: 6 })
+      .withMessage("Needs to be 6 or more characters")
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("User route");
+  }
+);
 
 module.exports = router;
